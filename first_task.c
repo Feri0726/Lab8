5462dc3dffac54c4244fe32d55fd10ee4aaf6dda
Feri0726 <feherferi2007@gmail.com>
2025-10-29 15:37:56 +0000

    first complete

--- first_task.c
+++ first_task.c
@@ -14,6 +14,16 @@ You can assume that the word is never longer than 99 characters.
 #include <stdio.h>
 
 int main(){
+  char array[100];
+  int i = 0;
+  printf("Give a char");
+  scanf("%s", array);
+  while (array[i] != '\0'){
+    printf("%c\n", array[i]);
+    i++;
+  }
+  
+
 
 
   return 0;
