aa6a08cae271e943a91bc7b09e5c6c7d6d8804b2
Feri0726 <feherferi2007@gmail.com>
2025-10-29 16:53:32 +0000

    third comp

--- third_task.c
+++ third_task.c
@@ -5,9 +5,23 @@ At the end the program should print the elements of the array backwards, and rel
 
 
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
 
 int main(){
-
-
-  return 0;
+int num;
+printf("Number of space");
+scanf("%d", &num);
+double *news = (double*)malloc((num)*sizeof(double));
+for (int i = 0; i < num; i++){
+  scanf("%lf", &news[i]);
+}
+  
+for(int i=num-1; i>=0; i--)
+{
+  printf("%lf ",news[i]);
+}
+return 0;
 }
